ffd3aeef62e10c001369e5c7a9f06477c9525e56
John Metzger <56820659+jometzg@users.noreply.github.com>
2024-08-07 09:28:45 +0100

    Create LightModel.cs

---
+++ plugins/LightModel.cs
@@ -0,0 +1,9 @@
+// Are really simple model of a light
+
+public class LightModel
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public bool IsOn { get; set; }
+    public int Brightness { get; set; }
+}
